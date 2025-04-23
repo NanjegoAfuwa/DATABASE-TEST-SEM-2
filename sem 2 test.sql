7dd9a38f873237a3ca1a52d812ed0984326cf3c0
NanjegoAfuwa <portablebirungi@gmail.com>
2025-04-23 11:42:51 +0300

    message

---
+++ sem 2 test.sql
@@ -0,0 +1,22 @@
+
+-- CREATE DATABASE my_work;
+-- CREATE TABLE Finance(
+--     StudentID int,
+--     Tuition_Fee int,
+--     Administartion_Fee int,
+--     Exams_Fee int,
+--     Medical_Fee 
+-- );
+
+-- adding data into the table--
+ 
+
+
+
+
+-- to extract all students-- 
+SELECT * FROM student
+
+
+    
+    
